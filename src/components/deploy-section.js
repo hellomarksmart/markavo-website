30fde66e498639dc34386a18581b2b45771272ed
nadia-fullstack <nadia@fullstack.ph>
2022-01-14 14:44:25 +0800

    pagination section [reusable]

--- src/components/deploy-section.js
+++ src/components/deploy-section.js
@@ -3,23 +3,36 @@ import {
   CloudUploadIcon,
   LockClosedIcon,
   RefreshIcon,
-} from '@heroicons/react/outline'
+} from "@heroicons/react/outline"
 
 const features = [
-  { name: 'Push to Deploy', icon: CloudUploadIcon, desc: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis' },
-  { name: 'SSL Certificates', icon: LockClosedIcon, desc: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis' },
-  { name: 'Simple Queues', icon: RefreshIcon, desc: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis' },
+  {
+    name: "Push to Deploy",
+    icon: CloudUploadIcon,
+    desc: "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis",
+  },
+  {
+    name: "SSL Certificates",
+    icon: LockClosedIcon,
+    desc: "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis",
+  },
+  {
+    name: "Simple Queues",
+    icon: RefreshIcon,
+    desc: "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis",
+  },
 ]
 
 const headings = [
   {
     title: "Deploy faster",
     heading: "Everything you need to deploy your app",
-    sub_heading: "Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend condimentum id viverra nulla.",
+    sub_heading:
+      "Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend condimentum id viverra nulla.",
   },
 ]
 
-export default function DeploySection() {
+const Deploy = () => {
   return (
     <div className="relative bg-white pt-16 sm:pt-24 lg:pt-32">
       <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
@@ -39,16 +52,21 @@ export default function DeploySection() {
 
         <div className="mt-12 bg-white">
           <div className="bg-white grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-            {features.map((feature) => (
+            {features.map(feature => (
               <div key={feature.name} className="pt-6">
                 <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
                   <div className="-mt-6">
                     <div>
                       <span className="inline-flex items-center justify-center p-3 bg-emerald-500 rounded-md shadow-lg">
-                        <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                        <feature.icon
+                          className="h-6 w-6 text-white"
+                          aria-hidden="true"
+                        />
                       </span>
                     </div>
-                    <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">{feature.name}</h3>
+                    <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                      {feature.name}
+                    </h3>
                     <p className="mt-5 text-base font-sans text-gray-500">
                       {feature.desc}
                     </p>
@@ -62,3 +80,5 @@ export default function DeploySection() {
     </div>
   )
 }
+
+export default Deploy
