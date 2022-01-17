7c69487ed1dc308a628fc9567ab31c102c37990c
nadia-fullstack <79129155+nadia-fullstack@users.noreply.github.com>
2022-01-17 10:06:12 +0800

    Two Price Service Page (#13)

---
+++ src/pages/two-price-service.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import TwoPrice from "../components/Two Price Service/index"
+
+const TwoPriceService = () => (
+  <Layout>
+    <Seo title="Home" />
+    <TwoPrice />
+  </Layout>
+)
+
+export default TwoPriceService
