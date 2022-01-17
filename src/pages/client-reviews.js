e7953ff019581ec3a5b4c79fe36387bbc455258d
nadia-fullstack <nadia@fullstack.ph>
2022-01-17 12:34:14 +0800

    Client Reviews Page

---
+++ src/pages/client-reviews.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import Reviews from "../components/Client Reviews/index"
+
+const ClientReviews = () => (
+  <Layout>
+    <Seo title="Home" />
+    <Reviews />
+  </Layout>
+)
+
+export default ClientReviews
