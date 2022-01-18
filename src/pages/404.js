64cb25f3b8d9eab8a7a01640592d889cedbe8f51
nadia-fullstack <79129155+nadia-fullstack@users.noreply.github.com>
2022-01-18 14:31:54 +0800

    404 Page (#20)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,46 @@
 import * as React from "react"
 
+import { Link } from "gatsby"
+
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div class="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+      <div class="max-w-max mx-auto">
+        <div class="sm:flex">
+          <p class="text-4xl font-extrabold text-emerald-600 sm:text-5xl">
+            404
+          </p>
+          <div class="sm:ml-6">
+            <div class="sm:border-l sm:border-gray-200 sm:pl-6">
+              <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                Page not found
+              </h1>
+              <p class="mt-1 text-base text-gray-500">
+                Please check the URL in the address bar and try again.
+              </p>
+            </div>
+            <div class="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+              <Link
+                to="/"
+                class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Go back home
+              </Link>
+              <Link
+                to="#"
+                class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-emerald-700 bg-emerald-100 hover:bg-emerald-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Contact support
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
