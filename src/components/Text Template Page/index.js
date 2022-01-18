b2428b6d674495f9fbe084984e292547e765a564
nadia-fullstack <nadia@fullstack.ph>
2022-01-18 15:49:13 +0800

    Text Template Page [WIP]

---
+++ src/components/Text Template Page/index.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import textTemplateSection from "../Text Template Page/text-template-section"
+
+const textTemplateComponent = () => (
+  <>
+    <textTemplateSection />
+  </>
+)
+
+export default textTemplateComponent
