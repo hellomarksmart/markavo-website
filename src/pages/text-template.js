e054b762f53af0ac76f6343f938545422fa38255
nadia-fullstack <79129155+nadia-fullstack@users.noreply.github.com>
2022-01-19 09:45:14 +0800

    Text Template Page - WIP (#23)

--- src/pages/text-template.js
+++ src/pages/text-template.js
@@ -2,12 +2,12 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import textTemplateComponent from "../components/Text Template Page/index"
+import TextTemplateComponent from "../components/Text Template Page/index"
 
 const TextTemplate = () => (
   <Layout>
     <Seo title="Text Template" />
-    <textTemplateComponent />
+    <TextTemplateComponent />
   </Layout>
 )
 
