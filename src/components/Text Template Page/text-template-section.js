b1d247ff6e05ea431956b6026637fb0d0a9d4b1d
nadia-fullstack <nadia@fullstack.ph>
2022-01-19 16:09:39 +0800

    Text Template Page

--- src/components/Text Template Page/text-template-section.js
+++ src/components/Text Template Page/text-template-section.js
@@ -10,6 +10,107 @@ const pageHeading = [
   },
 ]
 
+const listContents = [
+  {
+    text: "Quis elit egestas venenatis mattis dignissim.",
+  },
+  {
+    text: "Cras cras lobortis vitae vivamus ultricies facilisis tempus.",
+  },
+  {
+    text: "Orci in sit morbi dignissim metus diam arcu pretium.",
+  },
+]
+const blockquoteContent = [
+  {
+    quoteText:
+      "Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi.",
+  },
+]
+const figureContent = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3",
+    imageCaption: "Sagittis scelerisque nulla cursus in enim consectetur quam.",
+  },
+]
+const textContent = [
+  {
+    heading: "",
+    description:
+      "Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam porttitor ultrices gravida eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet, mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed elit interdum dignissim.",
+    subdescription: [
+      {
+        text: "Quis elit egestas venenatis mattis dignissim.",
+      },
+      {
+        text: "Cras cras lobortis vitae vivamus ultricies facilisis tempus.",
+      },
+      {
+        text: "Orci in sit morbi dignissim metus diam arcu pretium.",
+      },
+    ],
+    figureContent: [
+      {
+        image: "",
+        imageCaption: "",
+      },
+    ],
+    videoContent: [
+      {
+        link: "",
+        videoText: "",
+      },
+    ],
+  },
+  {
+    heading: "From beginner to expert in 30 days",
+    description:
+      "Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis diam.",
+    subdescription: [
+      {
+        text: "Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi.",
+      },
+    ],
+    figureContent: [
+      {
+        image:
+          "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3",
+        imageCaption:
+          "Sagittis scelerisque nulla cursus in enim consectetur quam.",
+      },
+    ],
+    videoContent: [
+      {
+        link: "",
+        videoText: "",
+      },
+    ],
+  },
+  {
+    heading: "Everything you need to get up and running",
+    description:
+      "Purus morbi dignissim senectus mattis adipiscing. Amet, massa quam varius orci dapibus volutpat cras. In amet eu ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut viverra ridiculus non molestie. Gravida quis fringilla amet eget dui tempor dignissim. Facilisis auctor venenatis varius nunc, congue erat ac. Cras fermentum convallis quam.",
+    subdescription: [
+      {
+        text: "",
+      },
+    ],
+    figureContent: [
+      {
+        image: "",
+        imageCaption: "",
+      },
+    ],
+    videoContent: [
+      {
+        link: "https://watch.videodelivery.net/8e908ac501a9266cf0c06dc0278e4b15",
+        videoText: "Video",
+      },
+    ],
+  },
+]
+
 const TextTemplateSection = () => {
   return (
     <div className="relative py-16 bg-white overflow-hidden">
@@ -133,78 +234,31 @@ const TextTemplateSection = () => {
           </div>
         ))}
         <div className="mt-6 text-lg max-w-prose mx-auto text-gray-500">
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong>{" "}
-            sed <strong>eget risus enim</strong>. Mattis mauris semper sed amet
-            vitae sed turpis id. Id dolor praesent donec est. Odio penatibus
-            risus viverra tellus varius sit neque erat velit. Faucibus commodo
-            massa rhoncus, volutpat. Dignissim sed eget risus enim.{" "}
-            <Link to="#">Mattis mauris semper</Link> sed amet vitae sed turpis
-            id.
-          </p>
-          <ul role="list">
-            <li>Quis elit egestas venenatis mattis dignissim.</li>
-            <li>
-              Cras cras lobortis vitae vivamus ultricies facilisis tempus.
-            </li>
-            <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-          </ul>
-          <p>
-            Quis semper vulputate aliquam venenatis egestas sagittis quisque
-            orci. Donec commodo sit viverra aliquam porttitor ultrices gravida
-            eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet,
-            mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed
-            elit interdum dignissim.
-          </p>
-          <h2>From beginner to expert in 30 days</h2>
-          <p>
-            Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat
-            in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum
-            mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
-            tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi.
-            Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis
-            diam.
-          </p>
-          <blockquote>
-            <p>
-              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
-              urna sed consectetur neque tristique pellentesque. Blandit amet,
-              sed aenean erat arcu morbi.
-            </p>
-          </blockquote>
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-            praesent donec est. Odio penatibus risus viverra tellus varius sit
-            neque erat velit.
-          </p>
-          <figure>
-            <img
-              className="w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3"
-              alt=""
-              width={1310}
-              height={873}
-            />
-            <figcaption>
-              Sagittis scelerisque nulla cursus in enim consectetur quam.
-            </figcaption>
-          </figure>
-          <h2>Everything you need to get up and running</h2>
-          <p>
-            Purus morbi dignissim senectus mattis <Link to="#">adipiscing</Link>
-            . Amet, massa quam varius orci dapibus volutpat cras. In amet eu
-            ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
-            viverra ridiculus non molestie. Gravida quis fringilla amet eget dui
-            tempor dignissim. Facilisis auctor venenatis varius nunc, congue
-            erat ac. Cras fermentum convallis quam.
-          </p>
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-            praesent donec est. Odio penatibus risus viverra tellus varius sit
-            neque erat velit.
-          </p>
+          {textContent.map(items => (
+            <div>
+              <h2>{items.heading}</h2>
+              <p>{items.description}</p>
+              <ul>
+                {items.subdescription.map(item => (
+                  <li>{item.text}</li>
+                ))}
+              </ul>
+              {items.figureContent.map(item => (
+                <figure>
+                  <img className="w-full rounded-lg" src={item.image} alt="" />
+                  <figcaption>{item.imageCaption}</figcaption>
+                </figure>
+              ))}
+              {items.videoContent.map(item => (
+                <Link
+                  to={item.link}
+                  className="text-sm font-medium text-emerald-400"
+                >
+                  {item.videoText}
+                </Link>
+              ))}
+            </div>
+          ))}
         </div>
       </div>
     </div>
