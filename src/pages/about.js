47ea065b31fc139f24601b378589993a7e0d513f
nadia-fullstack <79129155+nadia-fullstack@users.noreply.github.com>
2022-01-24 12:42:54 +0800

    Pages and Components Prismic Integration (#33)

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,106 @@
 import * as React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import About from "../components/About/index"
+import Banner from "../components/Reusable Sections/banner"
+import AboutSection from "../components/Reusable Sections/about-section"
+import Cta from "../components/Reusable Sections/cta-section"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="About" />
-    <About />
-  </Layout>
-)
+const About = props => {
+  const abouteData = props.data.prismicAboutUsPage.data
 
-export default IndexPage
+  return (
+    <Layout>
+      <Seo title="About" />
+      <Banner
+        heading={abouteData.banner_heading.text}
+        headingColored={abouteData.banner_heading_colored.text}
+        description={abouteData.banner_description.text}
+        buttonColored={abouteData.button_1_label.text}
+        buttonColoredLink={abouteData.button_1.url}
+        buttonPlain={abouteData.button_2_label.text}
+        buttonPlainLink={abouteData.button_2.url}
+      />
+      <AboutSection
+        heading={abouteData.about_section_heading.text}
+        description={abouteData.about_section_description.text}
+        about_items={abouteData.about_items}
+      />
+      <Cta
+        heading={abouteData.cta_heading.text}
+        headingSecond={abouteData.cta_heading_2.text}
+        description={abouteData.cta_description.text}
+        buttonPlain={abouteData.cta_button_label.text}
+        buttonPlainLink={abouteData.cta_button.url}
+      />
+    </Layout>
+  )
+}
+
+export default About
+
+export const AboutUsPageQuery = graphql`
+  query AboutUsPageQuery {
+    prismicAboutUsPage {
+      data {
+        banner_heading {
+          text
+        }
+        banner_heading_colored {
+          text
+        }
+        banner_description {
+          text
+        }
+        button_2_label {
+          text
+        }
+        button_1_label {
+          text
+        }
+        button_1 {
+          url
+        }
+        button_2 {
+          url
+        }
+        about_section_heading {
+          text
+        }
+        about_section_description {
+          text
+        }
+        about_items {
+          image {
+            url
+          }
+          name {
+            text
+          }
+          position {
+            text
+          }
+          description {
+            text
+          }
+        }
+        cta_heading {
+          text
+        }
+        cta_heading_2 {
+          text
+        }
+        cta_description {
+          text
+        }
+        cta_button_label {
+          text
+        }
+        cta_button {
+          url
+        }
+      }
+    }
+  }
+`
--- src/pages/payment-received.js
+++ src/pages/payment-received.js
@@ -1,14 +1,50 @@
 import * as React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import Payment from "../components/Payment Received/index"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <Payment />
-  </Layout>
-)
+const IndexPage = props => {
+  const paymentReceivedData = props.data.prismicPaymentReceived.data
+
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <div className="relative py-16 bg-white overflow-hidden">
+        <div className="relative px-4 sm:px-6 lg:px-8">
+          <div className="text-lg max-w-prose mx-auto">
+            <p className="mt-8 text-xl text-gray-500 leading-8">
+              {paymentReceivedData.message_1.text}
+            </p>
+            <p className="mt-8 text-xl text-gray-500 leading-8">
+              {paymentReceivedData.message_2.text}
+            </p>
+            <p className="mt-8 text-xl text-gray-500 leading-8">
+              {paymentReceivedData.message_3.text}
+            </p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const PaymentReceivedQuery = graphql`
+  query PaymentReceivedQuery {
+    prismicPaymentReceived {
+      data {
+        message_1 {
+          text
+        }
+        message_2 {
+          text
+        }
+        message_3 {
+          text
+        }
+      }
+    }
+  }
+`
