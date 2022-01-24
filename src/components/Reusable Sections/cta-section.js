cd76b43aa780293d8e6f7dc22ea440eb05c22c35
adi-fullstack <79141549+adi-fullstack@users.noreply.github.com>
2022-01-24 18:08:40 +0800

    qa polish on homepage (#36)

--- src/components/Reusable Sections/cta-section.js
+++ src/components/Reusable Sections/cta-section.js
@@ -10,17 +10,17 @@ const Cta = ({
 }) => {
   return (
     <div className="bg-emerald-400">
-      <div className="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
+      <div className="max-w-[90%] mx-auto text-center py-16 px-4 sm:py-16 sm:px-6 lg:px-8">
+        <h2 className="text-3xl font-extrabold text-white sm:text-[36px] pb-0">
           <span className="block">{heading}</span>
           <span className="block">{headingSecond}</span>
         </h2>
-        <p className="mt-4 text-lg leading-6 text-emerald-200">{description}</p>
+        <p className="mt-0 text-[18px] font-sans leading-6 text-emerald-200">{description}</p>
         {buttonPlainLink ? (
           <Link
             key={buttonPlain}
             to={buttonPlainLink}
-            className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-emerald-400 bg-white hover:bg-emerald-50 sm:w-auto"
+            className="mt-2 sm:w-11/12 mb-4 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-[16px] font-sans font-medium rounded-md text-emerald-400 bg-white hover:bg-emerald-50 sm:w-auto"
           >
             {buttonPlain}
           </Link>
--- src/components/Reusable Sections/deploy-section.js
+++ src/components/Reusable Sections/deploy-section.js
@@ -3,22 +3,22 @@ import { CloudUploadIcon } from "@heroicons/react/outline"
 
 const Deploy = ({ title, heading, description, cards }) => {
   return (
-    <div className="relative bg-white pt-16 sm:pt-24 lg:pt-32">
+    <div className="relative bg-white pt-16 pb-4 sm:pt-18 lg:pt-18">
       <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
         <div>
-          <h2 className="text-base font-semibold font-sans tracking-wider text-emerald-500 uppercase">
+          <h2 className="text-[16px] font-semibold font-sans tracking-wider text-emerald-500 uppercase mb-2">
             {title}
           </h2>
-          <p className="mt-2 text-3xl font-extrabold font-sans text-gray-900 tracking-tight sm:text-4xl">
+          <p className="mt-0 text-3xl font-extrabold font-sans text-gray-900 tracking-tight sm:text-[36px]">
             {heading}
           </p>
-          <p className="mt-5 max-w-prose mx-auto font-sans text-xl text-gray-500">
+          <p className="mt-5 max-w-prose mx-auto font-sans text-[20px] text-gray-500">
             {description}
           </p>
         </div>
 
-        <div className="mt-12 bg-white">
-          <div className="pt-6">
+        <div className="my-12 bg-white">
+          <div className="pt-0">
             <div className="bg-white grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
               {cards?.map((item, i) => {
                 return (
@@ -40,10 +40,10 @@ const Deploy = ({ title, heading, description, cards }) => {
                       </span>
                     </div>
                     {/* )} */}
-                    <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                    <h3 className="mt-8 mb-3 text-[18px] font-medium text-gray-900 tracking-tight">
                       {item?.title.text}
                     </h3>
-                    <p className="mt-5 text-base font-sans text-gray-500">
+                    <p className="mt-2 text-[16px] font-sans text-gray-500">
                       {item?.content.text}
                     </p>
                   </div>
