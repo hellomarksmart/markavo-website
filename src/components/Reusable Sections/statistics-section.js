bed2f0f2726fae6b2225849ff9a54f97af607594
adi-fullstack <adi@fullstack.ph>
2022-01-24 18:04:21 +0800

    qa polish on homepage

--- src/components/Reusable Sections/statistics-section.js
+++ src/components/Reusable Sections/statistics-section.js
@@ -11,42 +11,42 @@ const Statistics = ({
   statsRight,
 }) => {
   return (
-    <div className="bg-gray-50 pt-12 sm:pt-16">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="bg-gray-50 pt-10 sm:pt-14">
+      <div className="max-w-[90%] mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+          <h2 className="text-[36px] font-bold text-gray-900 sm:text-4xl">
             {heading}
           </h2>
-          <p className="mt-3 text-xl text-gray-500 sm:mt-4">{description}</p>
+          <p className="mt-3 text-[20px] font-normal font-sans text-gray-500 sm:mt-4">{description}</p>
         </div>
       </div>
-      <div className="mt-10 pb-12 bg-white sm:pb-16">
+      <div className="mt-10 pb-12 bg-white sm:pb-12">
         <div className="relative">
           <div className="absolute inset-0 h-1/2 bg-gray-50" />
           <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="max-w-4xl mx-auto">
               <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
-                <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
-                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
+                <div className="flex flex-col border-b border-gray-100 p-7 text-center sm:border-0 sm:border-r">
+                  <dt className="order-2 mt-1 font-sans text-[18px] leading-6 font-medium text-gray-500">
                     {nameLeft}
                   </dt>
-                  <dd className="order-1 text-5xl font-extrabold text-emerald-400">
+                  <dd className="order-1 mb-3 font-sans text-[48px] font-bold text-emerald-400">
                     {statsLeft}
                   </dd>
                 </div>
-                <div className="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
+                <div className="flex flex-col border-t border-b border-gray-100 p-7 text-center sm:border-0 sm:border-l sm:border-r">
+                  <dt className="order-2 mt-1 font-sans text-[18px] leading-6 font-medium text-gray-500">
                     {nameCenter}
                   </dt>
-                  <dd className="order-1 text-5xl font-extrabold text-emerald-400">
+                  <dd className="order-1 mb-3 font-sans text-[48px] font-bold text-emerald-400">
                     {statsCenter}
                   </dd>
                 </div>
-                <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
-                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
+                <div className="flex flex-col border-t border-gray-100 p-7 text-center sm:border-0 sm:border-l">
+                  <dt className="order-2 mt-1 font-sans text-[18px] leading-6 font-medium text-gray-500">
                     {nameRight}
                   </dt>
-                  <dd className="order-1 text-5xl font-extrabold text-emerald-400">
+                  <dd className="order-1 mb-3 font-sans text-[48px] font-bold text-emerald-400">
                     {statsRight}
                   </dd>
                 </div>
