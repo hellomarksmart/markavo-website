6efa7c7450df2622d361a0e9e0bc50660f0da32f
adi-fullstack <79141549+adi-fullstack@users.noreply.github.com>
2022-01-25 18:09:41 +0800

    qa fixes on clients reviews, services, about, banner (#41)
    * qa fixes on clients reviews, services, about, banner
    
    * about page polishing
    
    * fixed merging issues

--- src/components/Reusable Sections/services-section.js
+++ src/components/Reusable Sections/services-section.js
@@ -6,43 +6,45 @@ import { ArrowRightIcon } from "@heroicons/react/solid"
 const ServicesSection = ({ title, heading, description, service_items }) => {
   return (
     <div className="bg-white">
-      <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
+      <div className="max-w-7xl mx-auto pt-10 lg:pt-16 md:pt-10 px-4 sm:px-6 lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
         <div>
-          <h2 className="text-base font-semibold text-emerald-600 uppercase tracking-wide">
+          <h2 className="pt-1 text-[16px] font-sans font-semibold text-emerald-400 uppercase tracking-wide">
             {title}
           </h2>
-          <p className="mt-2 text-3xl font-extrabold text-gray-900">
+          <p className="mt-2 text-[30px] font-sans font-extrabold text-gray-900">
             {heading}
           </p>
-          <p className="mt-4 text-lg text-gray-500">{description}</p>
+          <p className="mt-4 text-[18px] font-sans text-gray-500">
+            {description}
+          </p>
         </div>
         <div className="mt-12 lg:mt-0 lg:col-span-2">
-          <dl className="space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:grid-rows-4 sm:grid-flow-col sm:gap-x-6 sm:gap-y-10 lg:gap-x-8">
+          <dl className="space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:grid-rows-4 sm:grid-flow-col sm:gap-x-6 sm:gap-y-10 lg:gap-x-8 font-sans">
             {service_items?.map((item, i) => {
               return (
                 <div key={i} className="relative">
                   <dt>
                     <CheckIcon
-                      className="absolute h-6 w-6 text-green-500"
+                      className="absolute h-6 w-6 text-emerald-400"
                       aria-hidden="true"
                     />
-                    <p className="ml-9 text-lg leading-6 font-medium text-gray-900">
+                    <p className="ml-9 mb-2 text-[18px] leading-6 text-gray-900">
                       {item?.title.text}
                     </p>
                   </dt>
-                  <dd className="mt-2 ml-9 text-base text-gray-500">
+                  <dd className="mt-2 ml-9 mb-2 text-[16px] text-gray-500">
                     {item?.content.text}
                   </dd>
-                  <dd className="ml-9 text-base">
+                  <dd className="ml-9 text-[16px] mb-0">
                     {item?.link.url ? (
                       <Link
                         key={item?.link.url}
                         to={item?.link.url}
-                        className="text-emerald-400 flex items-center hover:text-emerald-500"
+                        className="text-emerald-400 flex items-center font-semibold hover:text-emerald-500"
                       >
                         {item?.link_label.text}{" "}
                         <ArrowRightIcon
-                          className="h-6 w-6 pl-2"
+                          className="h-6 w-6 pl-2 pt-1"
                           aria-hidden="true"
                         />
                       </Link>
