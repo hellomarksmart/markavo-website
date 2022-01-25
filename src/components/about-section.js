90fe1663ea3c6870cc19d2f53c0f053af38de5f8
adi-fullstack <adi@fullstack.ph>
2022-01-25 19:11:02 +0800

    about page polished

--- src/components/about-section.js
+++ src/components/about-section.js
@@ -14,19 +14,19 @@ const AboutSection = ({ heading, description, about_items }) => {
             </p>
           </div>
           <div className="lg:col-span-2">
-            <ul className="space-y-12 sm:divide-y sm:divide-gray-200 sm:space-y-0 sm:-mt-8 lg:gap-x-8 lg:space-y-0">
+            <ul className="space-y-12 sm:divide-y sm:divide-gray-200 sm:space-y-0 sm:-mt-8 lg:gap-x-8 lg:space-y-0 ml-0">
               {about_items?.map((item, i) => {
                 return (
                   <li key={i} className="sm:py-[32px] mb-0">
-                    <div className="space-y-4 sm:space-y-0 sm:flex sm:flex-row">
-                      <div className="aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4 ">
+                    <div className="w-[100%] space-y-4 sm:space-y-0 sm:flex md:flex-row sm:flex-col">
+                      <div className="xl:w-[16rem] md:w-[15rem] sm:pb-6">
                         <img
-                          className="h-[332px] w-[250px] object-cover shadow-lg rounded-lg"
+                          className="sm:h-[332px] sm:w-[250px] w-[100%] h-[22rem] object-cover shadow-lg rounded-lg"
                           src={item?.image.url}
                           alt=""
                         />
                       </div>
-                      <div className="sm:col-span-2">
+                      <div className="sm:w-[66%] w-[100%] sm:col-span-2">
                         <div className="space-y-4 mt-0">
                           <div className="font-sans leading-6 font-medium space-y-1">
                             <h3 className="mb-1 text-[18px]">
@@ -37,7 +37,7 @@ const AboutSection = ({ heading, description, about_items }) => {
                             </p>
                           </div>
                           <div className="text-lg">
-                            <p className="font-sans text-gray-500 text-[18px]">
+                            <p className="font-sans text-gray-500 text-[18px] sm:-mt-[1rem]">
                               {item?.description.text}
                             </p>
                           </div>
