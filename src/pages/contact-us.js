eafd4ad972ad844b336e1b994dfcce651cf77a5b
nadia-fullstack <nadia@fullstack.ph>
2022-01-25 09:57:24 +0800

    Contact Us Page

---
+++ src/pages/contact-us.js
@@ -0,0 +1,72 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import ContactSection from "../components/Reusable Sections/contact-us-section"
+import Cta from "../components/Reusable Sections/cta-section"
+
+const Contact = props => {
+  const contactData = props.data.prismicContactUsPage.data
+
+  return (
+    <Layout>
+      <Seo title="Contact" />
+      <ContactSection
+        heading={contactData.heading.text}
+        description={contactData.description.text}
+        contact_items={contactData.contact_items}
+      />
+      <Cta
+        heading={contactData.cta_heading.text}
+        headingSecond={contactData.cta_heading_2.text}
+        description={contactData.cta_description.text}
+        buttonPlain={contactData.cta_button_label.text}
+        buttonPlainLink={contactData.cta_button.url}
+      />
+    </Layout>
+  )
+}
+
+export default Contact
+
+export const ContactUsPageQuery = graphql`
+  query ContactUsPageQuery {
+    prismicContactUsPage {
+      data {
+        heading {
+          text
+        }
+        description {
+          text
+        }
+        contact_items {
+          qr_code {
+            url
+          }
+          contact_title {
+            text
+          }
+          contact_details {
+            text
+          }
+        }
+        cta_heading {
+          text
+        }
+        cta_heading_2 {
+          text
+        }
+        cta_description {
+          text
+        }
+        cta_button_label {
+          text
+        }
+        cta_button {
+          url
+        }
+      }
+    }
+  }
+`
