11fc8be82cf3d45715a02c075d77ab8d8bc457c5
nadia-fullstack <79129155+nadia-fullstack@users.noreply.github.com>
2022-01-25 09:58:00 +0800

    Contact Us Page (#37)

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
           about_us_page: require("./src/schema/about-us.json"),
           not_found: require("./src/schema/not-found.json"),
           payment_received: require("./src/schema/payment-received.json"),
-          contact_us_page: {}
+          contact_us_page: require("./src/schema/contact-us.json"),
         },
       },
     },
