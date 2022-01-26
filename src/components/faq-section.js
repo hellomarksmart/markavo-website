26cb7d41e198441964441f17583fe08d73935763
nadia-fullstack <79129155+nadia-fullstack@users.noreply.github.com>
2022-01-26 16:23:57 +0800

    Fe qa issues (#54)
    * Fix QA Issues One Price Service Page
    
    * Fix Spacing One Price Service
    
    * Fix QA Issues Text Template
    
    * Fix Font Color

--- src/components/faq-section.js
+++ src/components/faq-section.js
@@ -4,7 +4,7 @@ const FAQs = ({ heading, faq_contents }) => {
   return (
     <div className="bg-gray-50">
       <div className="max-w-max mx-auto pt-12 pb-2 px-4 divide-y divide-gray-200 sm:px-6 lg:py-0 lg:px-8">
-        <h2 className="text-3xl font-extrabold text-gray-900">{heading}</h2>
+        <h2 className="text-[30px] font-extrabold text-gray-900">{heading}</h2>
         <div className="mt-8">
           <dl className="divide-y divide-gray-200">
             {faq_contents?.map((item, i) => {
@@ -13,11 +13,11 @@ const FAQs = ({ heading, faq_contents }) => {
                   key={i}
                   className="pt-6 pb-2 md:grid md:grid-cols-12 md:gap-8"
                 >
-                  <dt className="text-base font-sans font-medium text-gray-900 md:col-span-5">
+                  <dt className="text-[16px] font-medium text-gray-900 md:col-span-5">
                     {item?.faq_question.text}
                   </dt>
                   <dd className="mt-2 md:mt-0 md:col-span-7 mb-0 ">
-                    <p className="text-base font-sans text-gray-500">
+                    <p className="text-[16px] text-gray-500">
                       {item?.faq_answer.text}
                     </p>
                   </dd>
--- src/components/one-price-service-section.js
+++ src/components/one-price-service-section.js
@@ -27,10 +27,12 @@ const OnePriceServiceSection = ({
       <div className="pt-12 sm:pt-16 lg:pt-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center">
-            <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl lg:text-5xl">
+            <h2 className="text-[30px] sm:text-[36px] lg:text-[48px] font-extrabold text-gray-900">
               {heading}
             </h2>
-            <p className="mt-4 text-xl text-gray-600">{description}</p>
+            <p className="mt-4 text-xl text-gray-600 md:text-[20px]">
+              {description}
+            </p>
           </div>
         </div>
       </div>
@@ -40,20 +42,20 @@ const OnePriceServiceSection = ({
           <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="max-w-lg mx-auto rounded-lg shadow-lg overflow-hidden lg:max-w-none lg:flex">
               <div className="flex-1 bg-white px-6 py-8 lg:p-12">
-                <h3 className="text-2xl font-extrabold text-gray-900 sm:text-3xl">
+                <h3 className="text-[24px] font-extrabold text-gray-900 sm:text-[30px]">
                   {card_heading}
                 </h3>
-                <p className="mt-6 text-base text-gray-500">
+                <p className="mt-6 text-base text-gray-500 text-[16px]">
                   {card_description}
                 </p>
                 <div className="mt-8">
                   <div className="flex items-center">
-                    <h4 className="flex-shrink-0 pr-4 bg-white text-sm tracking-wider font-semibold uppercase text-emerald-600">
+                    <h4 className="flex-shrink-0 pr-4 bg-white text-[14px] tracking-wider font-semibold uppercase text-emerald-600 mb-0">
                       {card_subheading}
                     </h4>
                     <div className="flex-1 border-t-2 border-gray-200" />
                   </div>
-                  <ul className="mt-8 space-y-5 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-5">
+                  <ul className="mt-8 space-y-5 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-5 ml-0">
                     {card_features?.map((item, i) => (
                       <li key={i} className="flex items-start lg:col-span-1">
                         <div className="flex-shrink-0">
@@ -62,7 +64,7 @@ const OnePriceServiceSection = ({
                             aria-hidden="true"
                           />
                         </div>
-                        <p className="ml-3 text-sm text-gray-700">
+                        <p className="ml-3 text-[14px] text-gray-700">
                           {item?.feature.text}
                         </p>
                       </li>
@@ -71,19 +73,19 @@ const OnePriceServiceSection = ({
                 </div>
               </div>
               <div className="py-8 px-6 text-center bg-emerald-50 lg:flex-shrink-0 lg:flex lg:flex-col lg:justify-center lg:p-12">
-                <p className="text-lg leading-6 font-medium text-gray-900">
+                <p className="text-[18px] leading-6 font-medium text-gray-900 mb-0">
                   {right_card_subheading}
                 </p>
-                <div className="mt-4 flex items-center justify-center text-5xl font-extrabold text-gray-900">
+                <div className="mt-4 flex items-center justify-center text-[48px] font-extrabold text-gray-900">
                   <span>
                     {card_currency}
                     {card_price}
                   </span>
-                  <span className="ml-3 text-xl font-medium text-gray-500">
+                  <span className="ml-3 text-[20px] font-medium text-gray-500">
                     {card_currency_text}
                   </span>
                 </div>
-                <p className="mt-4 text-sm">
+                <p className="mt-4 text-[14px] mb-0">
                   {card_link ? (
                     <Link
                       key={card_link_label}
@@ -104,18 +106,18 @@ const OnePriceServiceSection = ({
                       <Link
                         key={card_buuton_label}
                         to={card_button_link}
-                        className="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-emerald-800 hover:bg-emerald-900"
+                        className="flex items-center justify-center px-5 py-3 border border-transparent text-[16px] font-medium rounded-md text-white bg-emerald-800 hover:bg-emerald-900"
                       >
                         {card_buuton_label}
                       </Link>
                     ) : (
-                      <div className="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-emerald-800 hover:bg-emerald-900">
+                      <div className="flex items-center justify-center px-5 py-3 border border-transparent text-[16px] font-medium rounded-md text-white bg-emerald-800 hover:bg-emerald-900">
                         {card_buuton_label}
                       </div>
                     )}
                   </div>
                 </div>
-                <div className="mt-4 text-sm">
+                <div className="mt-4 text-[14px]">
                   {card_sample_link ? (
                     <Link
                       key={card_sample}
