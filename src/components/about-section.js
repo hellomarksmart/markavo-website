941576b9c5c85a2d61e108ccc5a5ae52edaa8093
adi-fullstack <adi@fullstack.ph>
2022-01-26 16:21:51 +0800

    click up qa fixes

--- src/components/about-section.js
+++ src/components/about-section.js
@@ -3,37 +3,41 @@ import * as React from "react"
 const AboutSection = ({ heading, description, about_items }) => {
   return (
     <div className="bg-white">
-      <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
-        <div className="space-y-12 lg:grid lg:grid-cols-3 lg:gap-8 lg:space-y-0">
+      <div className="mx-auto py-0 px-4 max-w-7xl sm:px-6 lg:px-8 lg:pt-16">
+        <div className="space-y-12 xl:grid xl:grid-cols-3 xl:gap-8 xl:space-y-0">
           <div className="space-y-5 sm:space-y-4">
-            <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
+            <h2 className="text-3xl font-extrabold tracking-tight sm:text-[36px]">
               {heading}
             </h2>
-            <p className="text-xl text-gray-500">{description}</p>
+            <p className="  text-[20px] text-gray-500">
+              {description}
+            </p>
           </div>
           <div className="lg:col-span-2">
-            <ul className="space-y-12 sm:divide-y sm:divide-gray-200 sm:space-y-0 sm:-mt-8 lg:gap-x-8 lg:space-y-0">
+            <ul className="space-y-12 sm:divide-y sm:divide-gray-200 sm:space-y-0 sm:-mt-8 lg:gap-x-8 lg:space-y-0 ml-0">
               {about_items?.map((item, i) => {
                 return (
-                  <li key={i} className="sm:py-8">
-                    <div className="space-y-4 sm:grid sm:grid-cols-3 sm:items-start sm:gap-6 sm:space-y-0">
-                      <div className="aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4">
+                  <li key={i} className="sm:py-[32px] mb-0">
+                    <div className="w-[100%] space-y-4 sm:space-y-0 sm:flex md:flex-row sm:flex-col">
+                      <div className="xl:w-[16rem] md:w-[15rem] sm:pb-6">
                         <img
-                          className="object-cover shadow-lg rounded-lg"
+                          className="sm:h-[332px] sm:w-[250px] w-[100%] h-[22rem] object-cover shadow-lg rounded-lg"
                           src={item?.image.url}
                           alt=""
                         />
                       </div>
-                      <div className="sm:col-span-2">
-                        <div className="space-y-4">
-                          <div className="text-lg leading-6 font-medium space-y-1">
-                            <h3>{item?.name.text}</h3>
-                            <p className="text-emerald-600">
+                      <div className="sm:w-[66%] w-[100%] sm:col-span-2">
+                        <div className="space-y-4 mt-0">
+                          <div className="leading-6 font-medium space-y-1">
+                            <h3 className="mb-1 text-[18px]">
+                              {item?.name.text}
+                            </h3>
+                            <p className="text-emerald-600 text-[18px] pb-3">
                               {item?.position.text}
                             </p>
                           </div>
                           <div className="text-lg">
-                            <p className="text-gray-500">
+                            <p className="text-gray-500 text-[18px] sm:-mt-[1rem]">
                               {item?.description.text}
                             </p>
                           </div>
