32e8d18de2be89f3f5104309d4e773565f5bbe19
ariane-fullstack <ariane@fullstack.ph>
2022-01-27 15:08:02 +0800

    pricing section (wip)

---
+++ src/components/pricing-section.js
@@ -0,0 +1,80 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+const PricingSection = ({ pricingBody }) => {
+  console.log("data on pricing section:", pricingBody)
+
+  return (
+    <div className="bg-emerald-900 px-5">
+      <div className="max-w-[90%] mx-auto pt-12 px-4 sm:px-6 lg:px-8 lg:pt-20">
+        <div className="text-center">
+          <h2 className="text-[18px] leading-6 font-semibold text-emerald-300 uppercase tracking-wider">
+            {pricingBody[0].primary.eyebrow_headline.text}
+          </h2>
+          <p className="text-3xl font-bold text-white sm:text-4xl sm:max-w-md md:max-w-3xl lg:max-w-4xl lg:text-[48px] mx-auto">
+            {pricingBody[0].primary.title.text}
+          </p>
+          <p className="mt-3 max-w-4xl mx-auto text-base text-emerald-300 sm:mt-5 sm:text-[20px] md:text-[24px] md:max-w-[46rem] sm:max-w-md">
+            {pricingBody[0].primary.description.text}
+          </p>
+        </div>
+      </div>
+      <div className="mt-16 bg-emerald-900 pb-12 lg:mt-20 lg:pb-20">
+        <div id="PricingCards" className="flex items-center z-0">
+          {pricingBody[0].items.length === 3 && (
+            <>
+              {pricingBody[0].items.map((item, i) => {
+                return (
+                  <div id="PricingCard" className="flex-1 flex flex-col">
+                    <div className="bg-white py-10">
+                      <div>
+                        <h3 className="text-center text-[24px] font-medium text-gray-900">
+                          {item.plan_title.text}
+                        </h3>
+                        <div className="mt-4 flex items-center justify-center ">
+                          <span className="px-3 flex items-start text-6xl tracking-tight text-gray-900">
+                            {/* <span className="mt-2 mr-2 text-[36px] font-normal">
+                          $
+                        </span> */}
+                            <span className="font-bold text-[60px]">
+                              {item.price_option.text}
+                            </span>
+                          </span>
+                          {/* <span className="text-xl font-normal text-gray-500">
+                        /month
+                      </span> */}
+                        </div>
+                        <div className="border-t-2 border-gray-100 rounded-b-lg pt-10 pb-8 px-6 bg-gray-50 sm:px-10 sm:py-10">
+                          <div>{item.features.text}</div>
+                          <div className="mt-8">
+                            <div className="rounded-lg shadow-md">
+                              {item.call_to_action.url ? (
+                                <Link
+                                  to={item.call_to_action.url}
+                                  className="block w-full text-center rounded-lg border border-transparent bg-white px-6 py-3 text-[16px] font-medium text-emerald-600 hover:bg-gray-50"
+                                  aria-describedby="tier-hobby"
+                                >
+                                  {item.call_to_action_text.text}
+                                </Link>
+                              ) : (
+                                <div className="block w-full text-center rounded-lg border border-transparent bg-white px-6 py-3 text-base font-medium text-emerald-600 hover:bg-gray-50">
+                                  {item.call_to_action_text.text}
+                                </div>
+                              )}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )
+              })}
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PricingSection
