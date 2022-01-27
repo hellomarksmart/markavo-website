503b0fef3148b4b275605f2a72f40761a0675930
adi-fullstack <79141549+adi-fullstack@users.noreply.github.com>
2022-01-27 15:18:33 +0800

    fixed icon centering, font size (#60)

--- src/components/statistics-section.js
+++ src/components/statistics-section.js
@@ -14,7 +14,7 @@ const Statistics = ({
     <div className="bg-gray-50 pt-10 sm:pt-14">
       <div className="max-w-[90%] mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-3xl sm:text-[36px] font-bold text-gray-900 md:max-w-3xl sm:max-w-md sm:mx-auto sm:text-4xl">
+          <h2 className="text-3xl sm:text-[36px] font-bold text-gray-900 md:max-w-3xl sm:max-w-md sm:mx-auto">
             {heading}
           </h2>
           <p className="mt-3 text-base sm:text-[20px] font-normal text-gray-500 sm:mt-4 sm:max-w-md sm:mx-auto md:max-w-3xl">{description}</p>
