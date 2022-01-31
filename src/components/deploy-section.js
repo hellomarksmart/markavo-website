506c2fdb3888319dffa46f5ef2fa24cc8a23cf70
nadia-fullstack <79129155+nadia-fullstack@users.noreply.github.com>
2022-01-31 12:22:29 +0800

    Applied Color from figma and Fix Components (#69)
    * Applied color changes
    
    * Update Components

--- src/components/deploy-section.js
+++ src/components/deploy-section.js
@@ -5,7 +5,7 @@ const Deploy = ({ title, heading, description, cards }) => {
     <div className="relative bg-white pt-16 pb-4 sm:pt-18 lg:pt-18">
       <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
         <div className="max-w-[90%] mx-auto">
-          <h2 className="text-[16px] font-semibold tracking-wider text-emerald-500 uppercase mb-2">
+          <h2 className="text-[16px] font-bold tracking-wider text-emerald-500 uppercase mb-2">
             {title}
           </h2>
           <p className="mt-0 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-[36px] md:max-w-full sm:max-w-md sm:mx-auto">
@@ -31,7 +31,7 @@ const Deploy = ({ title, heading, description, cards }) => {
                           <span className="inline-flex items-center justify-center py-[.65rem] px-[0.7rem] bg-emerald-500 rounded-md shadow-lg">
                             <img
                               src={item?.icon.url}
-                              className="h-5 w-5 text-white mb-0 invert"
+                              className="h-5 w-5 text-gray-900 mb-0"
                               alt="icon"
                             />
                           </span>
@@ -39,7 +39,7 @@ const Deploy = ({ title, heading, description, cards }) => {
                       ) : (
                         ""
                       )}
-                      <h3 className="mt-8 mb-3 text-[18px] font-medium text-gray-900 tracking-tight">
+                      <h3 className="mt-8 mb-3 text-[18px] font-bold text-gray-900 tracking-tight">
                         {item?.title.text}
                       </h3>
                       <p className="mt-2 text-[16px] text-gray-500">
