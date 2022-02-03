9a31ec76876fce3bb6d12c79556e026455d17381
nadia-fullstack <79129155+nadia-fullstack@users.noreply.github.com>
2022-02-03 14:49:59 +0800

    Default Font Size (#100)
    * Fix Responsiveness of pricing section and Fix Warning
    
    * Default Font Sizes

--- src/components/About Us/about-section.js
+++ src/components/About Us/about-section.js
@@ -6,10 +6,10 @@ const AboutSection = ({ heading, description, about_items }) => {
       <div className="max-w-default mx-auto px-4 sm:px-6 lg:px-8 py-0 px-4 lg:pt-16">
         <div className="space-y-12 xl:grid xl:grid-cols-3 xl:gap-8 xl:space-y-0">
           <div className="space-y-5 sm:space-y-4">
-            <h2 className="text-3xl font-extrabold tracking-tight sm:text-3.5xl">
+            <h2 className="text-header2 font-extrabold tracking-tight sm:text-header2sm">
               {heading}
             </h2>
-            <p className="text-lg text-gray-500">{description}</p>
+            <p className="text-normalmd2 sm:text-normalmd text-gray-500">{description}</p>
           </div>
           <div className="lg:col-span-2">
             <ul className="space-y-12 sm:divide-y sm:divide-gray-200 sm:space-y-0 sm:-mt-8 lg:gap-x-8 lg:space-y-0 ml-0">
@@ -21,21 +21,21 @@ const AboutSection = ({ heading, description, about_items }) => {
                         <img
                           className="sm:h-imageH sm:w-imageW w-full h-imageH_sm object-cover shadow-lg rounded-lg"
                           src={item?.image.url}
-                          alt={item?.image.alt}
+                          alt={item?.image.alt ? item?.image.alt : ""}
                         />
                       </div>
                       <div className="w-full md:w-twoThirds w-full sm:col-span-2">
                         <div className="space-y-4 mt-0">
                           <div className="leading-6 font-medium space-y-1">
-                            <h3 className="mb-1 text-base">
+                            <h3 className="mb-1 text-header3sm sm:text-header3">
                               {item?.name.text}
                             </h3>
-                            <p className="text-emerald-600 text-base pb-3">
+                            <p className="text-emerald-600 text-normalsm2 sm:text-normalsm pb-3">
                               {item?.position.text}
                             </p>
                           </div>
-                          <div className="text-lg">
-                            <p className="text-gray-500 text-base sm:-mt-[1rem]">
+                          <div>
+                            <p className="text-gray-500 text-normalmd2 sm:text-normalmd sm:-mt-[1rem]">
                               {item?.description.text}
                             </p>
                           </div>
