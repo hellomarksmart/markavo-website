5cd0c8dc5483037e67a3b5154a40bc39ab543af6
adi-fullstack <79141549+adi-fullstack@users.noreply.github.com>
2022-02-22 13:27:38 +0800

    fixed missing texts and added video embed (#130)

--- src/templates/payment-received.js
+++ src/templates/payment-received.js
@@ -32,9 +32,15 @@ const PaymentReceived = ({ data }) => {
             <p className="mt-8 text-xl text-gray-500 leading-8">
               {paymentReceivedData.message_1.text}
             </p>
-            <p className="mt-8 text-xl text-gray-500 leading-8">
-              {paymentReceivedData.message_2.text}
-            </p>
+            <div className="iframe-holder">
+              <iframe
+                id="iframeID"
+                src={paymentReceivedData.video_link.url}
+                title={paymentReceivedData.message_2.text}
+                allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+                allowfullscreen="true"
+              ></iframe>
+            </div>
             <p className="mt-8 text-xl text-gray-500 leading-8">
               {paymentReceivedData.message_3.text}
             </p>
@@ -64,6 +70,9 @@ export const PaymentReceivedQuery = graphql`
         }
         message_2 {
           text
+        }
+        video_link {
+          url
         }
         message_3 {
           text
