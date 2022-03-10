45908f36e5aba8d456496080968b460f4e295ea9
adi-fullstack <79141549+adi-fullstack@users.noreply.github.com>
2022-03-10 17:25:59 +0800

    fixed robots txt (#164) (#165)

--- src/templates/all-services.js
+++ src/templates/all-services.js
@@ -26,8 +26,6 @@ const AllServices = ({ data }) => {
       headerBody={data.prismicHeader}
       footerBody={data.prismicFooter}
     >
-      <link rel="alternate" hreflang="en" href="https://markavowebsite.gatsbyjs.io" />
-      <link rel="alternate" hreflang="es" href="https://markavowebsite.gatsbyjs.io" />
       <Seo title="Services" />
       <ServicesSection
         title={servcicesData.service_section_title.text}
