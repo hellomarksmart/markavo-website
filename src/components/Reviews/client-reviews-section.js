c19f891965f7ba020b1b005511f421621d289ae4
adi-fullstack <adi@fullstack.ph>
2022-03-24 19:16:32 +0800

    testing

--- src/components/Reviews/client-reviews-section.js
+++ src/components/Reviews/client-reviews-section.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import { StarIcon } from "@heroicons/react/solid"
 import Pagination from "../Reusable Components/pagination-section"
 import { ArticleJsonLd } from 'gatsby-plugin-next-seo';
+import { Helmet } from "react-helmet";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ")
@@ -23,113 +24,61 @@ const ClientReviewsSection = ({ reviews }) => {
 
   return (
     <>
-      <ArticleJsonLd
-        url='https://markavo.com/markavo-reviews'
-        title="Review"
-        overrides={{
-          review: [{
-            itemReviewed: {
-              "@type": "Organization",
-              name: "Markavo",
-              images: "https://markavo.com/markavo-reviews/markavo-reviews"
-            },
-            author: {
-              "@type": "Person",
-              name: "Anonymous",
-              url: 'https://markavo.com/markavo-reviews/markavo-reviews'
-            },
-            publisher: {
-              "@type": "Organization",
-              name: "markavowebsitestaging.gatsbyjs.io",
-              sameAs: "https://markavo.com/markavo-reviews",
-            },
-            datePublished: "2022-03-22 14:08:00",
-            reviewBody: "Very Helpful, answers all my Questions.",
-            reviewRating: {
-              "@type": "Rating",
-              bestRating: "5",
-              worstRating: "1",
-              ratingValue: "5",
-            }
-          },
-          {
-            itemReviewed: {
-              "@type": "Organization",
-              name: "Markavo",
-              images: "https://markavo.com/markavo-reviews/markavo-reviews"
-            },
-            author: {
-              "@type": "Person",
-              name: "Tom",
-              url: 'https://markavo.com/markavo-reviews/markavo-reviews'
-            },
-            publisher: {
-              "@type": "Organization",
-              name: "markavowebsitestaging.gatsbyjs.io",
-              sameAs: "https://markavo.com/markavo-reviews",
-            },
-            datePublished: "2022-03-22 14:08:00",
-            reviewBody: "Very professional and easy to work with.",
-            reviewRating: {
-              "@type": "Rating",
-              bestRating: "5",
-              worstRating: "1",
-              ratingValue: "5",
-            }
-          },
-          {
-            itemReviewed: {
-              "@type": "Organization",
-              name: "Markavo",
-              images: "https://markavo.com/markavo-reviews/markavo-reviews"
-            },
-            author: {
-              "@type": "Person",
-              name: "J Alexander Hayes",
-              url: 'https://markavo.com/markavo-reviews/markavo-reviews'
-            },
-            publisher: {
-              "@type": "Organization",
-              name: "markavowebsitestaging.gatsbyjs.io",
-              sameAs: "https://markavo.com/markavo-reviews",
-            },
-            datePublished: "2022-03-22 14:08:00",
-            reviewBody: "I really like the constant communication about pending actions. They have been very helpful throughout this entire process. I highly recommend!",
-            reviewRating: {
-              "@type": "Rating",
-              bestRating: "5",
-              worstRating: "1",
-              ratingValue: "5",
-            }
-          },
-          {
-            itemReviewed: {
-              "@type": "Organization",
-              name: "Markavo",
-              images: "https://markavo.com/markavo-reviews/markavo-reviews"
-            },
-            author: {
-              "@type": "Person",
-              name: "Amy Ouzoonian",
-              url: 'https://markavo.com/markavo-reviews/markavo-reviews'
-            },
-            publisher: {
-              "@type": "Organization",
-              name: "markavowebsitestaging.gatsbyjs.io",
-              sameAs: "https://markavo.com/markavo-reviews",
-            },
-            datePublished: "2022-03-22 14:08:00",
-            reviewBody: "I have had a great experience with them and the attorney who is handling my trademark. They are so helpful, professional, and go the extra mile to make sure that you get your trademark registered correctly. I am so grateful that I decided to work with this company to get my trademark registered.",
-            reviewRating: {
-              "@type": "Rating",
-              bestRating: "5",
-              worstRating: "1",
-              ratingValue: "5",
-            }
-          },
-          ]
-        }}
-      />
+      <Helmet>
+        <StructuredData>
+          {{
+            "@context": "https://schema.org",
+            "@type": "Reviews",
+            "@id": "https://markavo.com/markavo-reviews",
+            name: "Markavo",
+            url: "https://markavo.com",
+            review: [
+              {
+                "@type": "Review",
+                itemReviewed: {
+                  "@type": "Organization",
+                  name: "Markavo",
+                },
+                author: { "@type": "Person", name: "Anonymous" },
+                publisher: {
+                  "@type": "Organization",
+                  name: "markavowebsitestaging.gatsbyjs.io",
+                  sameAs: "https://markavowebsitestaging.gatsbyjs.io/",
+                },
+                datePublished: "2022-03-22 14:08:00",
+                reviewBody: "Very Helpful, answers all my Questions.",
+                reviewRating: {
+                  "@type": "Rating",
+                  bestRating: "5",
+                  worstRating: "1",
+                  ratingValue: "5",
+                },
+              },
+              {
+                "@type": "Review",
+                itemReviewed: {
+                  "@type": "Organization",
+                  name: "Markavo",
+                },
+                author: { "@type": "Person", name: "Anonymous" },
+                publisher: {
+                  "@type": "Organization",
+                  name: "markavowebsitestaging.gatsbyjs.io",
+                  sameAs: "https://markavowebsitestaging.gatsbyjs.io/",
+                },
+                datePublished: "2022-03-22 14:08:00",
+                reviewBody: "Very Helpful, answers all my Questions.",
+                reviewRating: {
+                  "@type": "Rating",
+                  bestRating: "5",
+                  worstRating: "1",
+                  ratingValue: "5",
+                },
+              },
+            ],
+          }}
+        </StructuredData>
+      </Helmet>
       <div className="bg-white">
         <div className="ml-0 pb-0 px-4 sm:pb-0">
           <div
