23c78a4b4f6c2461523f1c06cea631779715d477
Ariane <65338049+ariane-fullstack@users.noreply.github.com>
2022-03-28 13:09:31 +0800

    Amendments/microformat (#236)
    * structured data WIP
    
    * structured json
    
    Co-authored-by: adi-fullstack <adi@fullstack.ph>

--- src/components/Reviews/client-reviews-section.js
+++ src/components/Reviews/client-reviews-section.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react"
 import { StarIcon } from "@heroicons/react/solid"
 import Pagination from "../Reusable Components/pagination-section"
-import { ArticleJsonLd } from 'gatsby-plugin-next-seo';
+import { ArticleJsonLd } from "gatsby-plugin-next-seo"
+import { data } from "autoprefixer"
 
+import { Helmet } from "react-helmet"
+// import { StructuredData } from "../../components/structured-data"
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ")
 }
@@ -21,30 +24,52 @@ const ClientReviewsSection = ({ reviews }) => {
   const startIndex = currentPage * 50 - 50
   const endIndex = startIndex + 50
 
+  let review = []
+  
+  console.log(review);
+
   return (
     <>
       <div className="bg-white">
         <div className="ml-0 pb-0 px-4 sm:pb-0">
-          <div
-            className="max-w-default mx-auto px-4 sm:px-6 lg:px-8 mt-0 pb-10 border-b border-gray-200 divide-y divide-gray-200"
-          >
+          <div className="max-w-default mx-auto px-4 sm:px-6 lg:px-8 mt-0 pb-10 border-b border-gray-200 divide-y divide-gray-200">
             {dataReviews?.slice(startIndex, endIndex).map((item, i) => {
+              const newitem = {
+                "@type": "Review",
+                "reviewRating": {
+                  "@type": "Rating",
+                  "ratingValue": `${item.reviewer_message.text}`,
+                  "bestRating": "THIS IS INSIDE",
+                  "worstRating": "1"
+                },
+                "itemReviewed": {
+                  "@type": "Organization",
+                  "name": "Markavo",
+                  "images": "https://markavo.com/markavo-reviews/markavo-reviews"
+                },
+                "publisher": {
+                  "@type": "Organization",
+                  "name": "markavowebsitestaging.gatsbyjs.io",
+                  "sameAs": "https://markavo.com/markavo-reviews"
+                },
+                "datePublished": "2022-03-22 14:08:00",
+                "author": {
+                  "@type": "Person",
+                  "name": `${item.reviewer_name.text}`
+                },
+                "reviewBody": `${item.reviewer_message.text}`
+              }
+              review.push(newitem)
               return (
                 <div key={i} className="sm:py-6 py-5">
                   <div className="mt-6 flex items-start text-sm lg:mt-0 lg:col-start-1 lg:col-span-4 lg:row-start-1 sm-flex-col sm:items-start xl:col-span-3">
-                    <p
-                    >
-                      <span
-                        className="font-bold text-gray-900 text-xs sm:mb-5.5 mb-0 pl-1"
-                      >
+                    <p>
+                      <span className="font-bold text-gray-900 text-xs sm:mb-5.5 mb-0 pl-1">
                         {item?.reviewer_name.text}
                       </span>
                     </p>
-                    <div
-                    >
-                      <div
-                        className="flex items-center pl-2"
-                      >
+                    <div>
+                      <div className="flex items-center pl-2">
                         {[0, 1, 2, 3, 4].map(reviewer_rate => (
                           <StarIcon
                             key={reviewer_rate}
@@ -54,16 +79,15 @@ const ClientReviewsSection = ({ reviews }) => {
                                 : "text-gray-200",
                               "h-5 w-5 flex-shrink-0"
                             )}
-                            aria-hidden="true" />
+                            aria-hidden="true"
+                          />
                         ))}
                       </div>
                     </div>
                   </div>
                   <div className="lg:col-start-5 lg:col-span-8 xl:col-start-4 xl:col-span-9 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:items-start">
                     <div className="mt-4 lg:mt-6 xl:mt-0 xl:col-span-2">
-                      <h3
-                        className="font-medium text-xs text-gray-900 mb-0"
-                      >
+                      <h3 className="font-medium text-xs text-gray-900 mb-0">
                         {item?.reviewer_message.text}
                       </h3>
                     </div>
@@ -78,11 +102,21 @@ const ClientReviewsSection = ({ reviews }) => {
             currentPage={currentPage}
             pages={pages}
             totalItems={responseLength}
-            setCurrentPage={setCurrentPage} />
+            setCurrentPage={setCurrentPage}
+          />
         ) : (
           ""
         )}
-      </div></>
+      </div>
+      <Helmet>
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org/",
+            review
+          })}
+        </script>
+      </Helmet>
+    </>
   )
 }
 
