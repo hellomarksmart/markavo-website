3a53b4aa7f1ab4aaa5c0de061f119442122bba02
Geraldine Biloy <geraldine@fullstack.ph>
2022-07-19 16:54:04 +0800

    add script tags

--- src/html.js
+++ src/html.js
@@ -1,7 +1,5 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { navigate } from "gatsby"
-
 export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
@@ -21,6 +19,29 @@ export default function HTML(props) {
         `,
           }}
         />
+        <script type="text/javascript">
+          {`
+          window.SMITH={}
+          ,window.SMITH.smithChatAccount="bbaa9b5f-3188-48b6-ac1d-920381a0e491",window.SMITH.baseUrl="https://app.smith.ai";
+          var script=document.createElement("script"); script.async=!0,
+          script.type="text/javascript",script.src="https://app.smith.ai/chat/widget-latest.js",document.getElementsByTagName("HEAD").item(0).appendChild(script);
+          `}
+        </script>
+        <script>
+          {`
+            window.ap3c = window.ap3c || {};
+            var ap3c = window.ap3c;
+            ap3c.cmd = ap3c.cmd || [];
+            ap3c.cmd.push(function() {
+            ap3c.init('Yrt10H7Kx7zwJqHrbWFya2F2b2NvbQ', 'https://capture-api.autopilotapp.com/');
+            ap3c.track({v: 0});
+            });
+          `}
+          var s, t; s = document.createElement('script'); s.type =
+          'text/javascript'; s.src = "https://cdn3l.ink/app.js"; t =
+          document.getElementsByTagName('script')[0];
+          t.parentNode.insertBefore(s, t);
+        </script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
